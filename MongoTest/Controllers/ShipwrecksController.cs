ff23e3de28cac1fb18b54a450a24dc416aa3561a
Leticia-Mendes <mendesz.leticia@gmail.com>
2022-03-30 00:18:19 -0300

    versao dotnet 3

---
+++ MongoTest/Controllers/ShipwrecksController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace MongoTest.Controllers
+{
+    //http://localhost:5000/swagger/index.html
+    //http://localhost:5000/api/Shipwrecks
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShipwrecksController : ControllerBase
+    {
+        private readonly IMongoCollection<Shipwreck> _shipwreckCollection;
+
+        public ShipwrecksController(IMongoClient client)
+        {
+            var database = client.GetDatabase("sample_geospatial");
+            _shipwreckCollection = database.GetCollection<Shipwreck>("shipwrecks");
+        }
+
+        [HttpGet]
+        public IEnumerable<Shipwreck> Get()
+        {
+            return _shipwreckCollection.Find(s => s.FeatureType == "Wrecks - Visible").ToList();
+        }
+    }
+}
